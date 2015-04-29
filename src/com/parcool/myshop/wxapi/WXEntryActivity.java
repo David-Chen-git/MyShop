bd88a1a270de42705977fe761793234650adcfcf
parcool <393014752@qq.com>
2015-04-29 15:36:19 +0800

    添加友盟集成配置

---
+++ src/com/parcool/myshop/wxapi/WXEntryActivity.java
@@ -0,0 +1,12 @@
+package com.parcool.myshop.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+	@Override
+	public void onStart() {
+
+	}
+
+}
