a5c82b373475cdb9ceee1116f9aa538bdbf47834
parcool <393014752@qq.com>
2015-04-29 16:16:47 +0800

    新增DialogUtil

---
+++ src/com/parcool/myshop/utils/DialogUtil.java
@@ -0,0 +1,269 @@
+package com.parcool.myshop.utils;
+
+
+import com.parcool.myshop.R;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+
+public class DialogUtil {
+	// 防止被实例化
+	private DialogUtil() {
+	}
+
+	// 定义私有实例
+	private static DialogUtil util = null;
+
+	// 对外公开的获取实例方法
+	public static DialogUtil getInstance() {
+		if (util == null) {
+			util = new DialogUtil();
+		}
+		return util;
+	}
+
+	private ProgressBar pb;
+	private RelativeLayout viewGroupRl;
+	private LinearLayout viewGroupLl;
+	private FrameLayout viewGroupFl;
+	private ProgressDialog pd;
+	private DisplayMetrics displayMeterics = new DisplayMetrics();
+	private int backCount = 0;
+	private Activity dismissActivity = null;
+
+	
+	/***
+	 * 显示单独的progressBar.如果要设置某个例外，那么也设置这个view的tag为“gone”或“invisible”
+	 * @param activity
+	 * @param viewGroupResId 父容器资源ID。只能是LinearLayout或者RelativeLayout或者FrameLayout
+	 */
+	public void showSingelProgressBar(Activity activity, int viewGroupResId) {
+		View tempView = null;
+		if (activity.findViewById(viewGroupResId) instanceof RelativeLayout) {
+			viewGroupRl = (RelativeLayout) activity.findViewById(viewGroupResId);
+			for (int i = 0; i < viewGroupRl.getChildCount(); i++) {
+				tempView = viewGroupRl.getChildAt(i);
+				if (tempView.getTag() == null || (tempView.getVisibility() == View.GONE && tempView.getTag().toString().equals("gone")) || (tempView.getVisibility() == View.INVISIBLE && tempView.getTag().toString().equals("invisible"))) {
+					tempView.setVisibility(View.GONE);
+				}
+			}
+			RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+			layoutParams.topMargin = 100;
+			pb = new ProgressBar(activity);
+			pb.setLayoutParams(layoutParams);
+			pb.setIndeterminateDrawable(activity.getResources().getDrawable(R.drawable.circle_progressbar));
+			viewGroupRl.addView(pb);
+		} else if (activity.findViewById(viewGroupResId) instanceof LinearLayout) {
+			viewGroupLl = (LinearLayout) activity.findViewById(viewGroupResId);
+			for (int i = 0; i < viewGroupLl.getChildCount(); i++) {
+				tempView = viewGroupLl.getChildAt(i);
+				if (tempView.getTag() == null || (tempView.getVisibility() == View.GONE && tempView.getTag().toString().equals("gone")) || (tempView.getVisibility() == View.INVISIBLE && tempView.getTag().toString().equals("invisible"))) {
+					tempView.setVisibility(View.GONE);
+				}
+			}
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+			layoutParams.topMargin = 100;
+			pb = new ProgressBar(activity);
+			pb.setLayoutParams(layoutParams);
+			pb.setIndeterminateDrawable(activity.getResources().getDrawable(R.drawable.circle_progressbar));
+			viewGroupLl.addView(pb);
+		} else if (activity.findViewById(viewGroupResId) instanceof FrameLayout) {
+			viewGroupFl = (FrameLayout) activity.findViewById(viewGroupResId);
+			for (int i = 0; i < viewGroupFl.getChildCount(); i++) {
+				tempView = viewGroupFl.getChildAt(i);
+				if (tempView.getTag() == null || (tempView.getVisibility() == View.GONE && tempView.getTag().toString().equals("gone")) || (tempView.getVisibility() == View.INVISIBLE && tempView.getTag().toString().equals("invisible"))) {
+					tempView.setVisibility(View.GONE);
+				}
+			}
+			FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+			layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+			layoutParams.topMargin = 100;
+			pb = new ProgressBar(activity);
+			pb.setLayoutParams(layoutParams);
+			pb.setIndeterminateDrawable(activity.getResources().getDrawable(R.drawable.circle_progressbar));
+			viewGroupFl.addView(pb);
+		}
+		tempView = null;
+
+	}
+
+	/***
+	 * 隐藏progressBar
+	 */
+	public void dismissProgressBar() {
+		backCount = 0;
+		//设计dismissActivity是因为有可能报错：
+		//java.lang.IllegalArgumentException: View not attached to window manager
+		if (pd != null && pd.isShowing() && dismissActivity != null && !dismissActivity.isFinishing()) {
+			pd.dismiss();
+			pd = null;
+			return;
+		}
+		dismissActivity = null;
+		if (viewGroupRl != null) {
+			if (pb != null) {
+				viewGroupRl.removeView(pb);
+			}
+			for (int i = 0; i < viewGroupRl.getChildCount(); i++) {
+				if (viewGroupRl.getChildAt(i).getTag() == null || (!viewGroupRl.getChildAt(i).getTag().toString().equals("gone") && !viewGroupRl.getChildAt(i).getTag().toString().equals("invisible"))) {
+					viewGroupRl.getChildAt(i).setVisibility(View.VISIBLE);
+				}
+			}
+			viewGroupRl = null;
+		} else if (viewGroupLl != null) {
+			if (pb != null) {
+				viewGroupLl.removeView(pb);
+			}
+			for (int i = 0; i < viewGroupLl.getChildCount(); i++) {
+				if (viewGroupLl.getChildAt(i).getTag() == null || (!viewGroupLl.getChildAt(i).getTag().toString().equals("gone") && !viewGroupLl.getChildAt(i).getTag().toString().equals("invisible"))) {
+					viewGroupLl.getChildAt(i).setVisibility(View.VISIBLE);
+				}
+			}
+			viewGroupLl = null;
+		} else if (viewGroupFl != null) {
+			if (pb != null) {
+				viewGroupFl.removeView(pb);
+			}
+			for (int i = 0; i < viewGroupFl.getChildCount(); i++) {
+				if (viewGroupFl.getChildAt(i).getTag() == null || (!viewGroupFl.getChildAt(i).getTag().toString().equals("gone") && !viewGroupFl.getChildAt(i).getTag().toString().equals("invisible"))) {
+					viewGroupFl.getChildAt(i).setVisibility(View.VISIBLE);
+				}
+			}
+			viewGroupFl = null;
+		}
+
+	}
+
+	// 获取屏幕宽度
+	public int getPhoneWidth(Activity activity) {
+		if (displayMeterics == null) {
+			WindowManager wm = activity.getWindowManager();
+			wm.getDefaultDisplay().getMetrics(displayMeterics);
+		}
+		return displayMeterics.widthPixels;
+	}
+
+	// 获取屏幕高度
+	public int getPhoneHeight(Activity activity) {
+		if (displayMeterics == null) {
+			WindowManager wm = activity.getWindowManager();
+			wm.getDefaultDisplay().getMetrics(displayMeterics);
+		}
+		return displayMeterics.heightPixels;
+	}
+
+	/***
+	 * 创建progressDialog
+	 * 
+	 * @param context
+	 */
+	public void showProgressDialog(Context context) {
+		pd = new ProgressDialog(context);
+		pd.setIndeterminateDrawable(context.getResources().getDrawable(R.drawable.circle_progressbar));
+		pd.show();
+	}
+
+
+	/***
+	 * 创建progressDialog
+	 * 
+	 * @param context
+	 */
+	public void showProgressDialog(Context context, String msg) {
+		pd = new ProgressDialog(context);
+		pd.setIndeterminateDrawable(context.getResources().getDrawable(R.drawable.circle_progressbar));
+		pd.setMessage(msg);
+		pd.show();
+	}
+
+	/***
+	 * 创建progressDialog
+	 * 
+	 * @param context
+	 */
+	public void showProgressDialog(final Activity activity, boolean cancelable) {
+		dismissActivity = activity;
+		backCount = 0;
+		pd = new ProgressDialog(activity);
+		pd.setIndeterminateDrawable(activity.getResources().getDrawable(R.drawable.circle_progressbar));
+		pd.setCancelable(cancelable);
+		if (!cancelable) {
+			pd.setOnKeyListener(new DialogInterface.OnKeyListener() {
+
+				@Override
+				public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+					// TODO Auto-generated method stub
+					if (event.getAction() == KeyEvent.ACTION_DOWN) {
+						if (keyCode == KeyEvent.KEYCODE_BACK) {
+							if (backCount > 1) {
+								activity.finish();
+								return false;
+							} else if (backCount == 0) {
+								Toast.makeText(activity, "正在操作中，强行返回请再按2次返回键!", Toast.LENGTH_SHORT).show();
+							} else if (backCount == 1) {
+								Toast.makeText(activity, "正在操作中，强行返回请再按1次返回键!", Toast.LENGTH_SHORT).show();
+							}
+							backCount++;
+						}
+					}
+					return false;
+				}
+			});
+		}
+		pd.show();
+	}
+
+	/***
+	 * 创建progressDialog
+	 * 
+	 * @param context
+	 */
+	public void showProgressDialog(final Activity activity, String msg, boolean cancelable) {
+		dismissActivity = activity;
+		backCount = 0;
+		pd = new ProgressDialog(activity);
+		pd.setIndeterminateDrawable(activity.getResources().getDrawable(R.drawable.circle_progressbar));
+		pd.setMessage(msg);
+		pd.setCancelable(cancelable);
+		if (!cancelable) {
+			pd.setOnKeyListener(new DialogInterface.OnKeyListener() {
+
+				@Override
+				public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+					// TODO Auto-generated method stub
+					if (event.getAction() == KeyEvent.ACTION_DOWN) {
+						if (backCount > 1) {
+							dismissProgressBar();
+							activity.finish();
+							return false;
+						} else if (backCount == 0) {
+							Toast.makeText(activity, "正在操作中，强行返回请再按2次返回键!", Toast.LENGTH_SHORT).show();
+						} else if (backCount == 1) {
+							Toast.makeText(activity, "正在操作中，强行返回请再按1次返回键!", Toast.LENGTH_SHORT).show();
+						}
+						backCount++;
+					}
+					return false;
+				}
+			});
+		}
+		if (!activity.isFinishing()) {
+			pd.show();
+		}
+	}
+}
---
+++ src/com/parcool/myshop/view/DotCircleProgressView.java
@@ -0,0 +1,135 @@
+package com.parcool.myshop.view;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+
+
+import com.parcool.myshop.R;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class DotCircleProgressView extends View {
+
+	private Paint mPaint;
+	private int dotColor = Color.BLUE;// 圆点颜色，设置默认值为:Color.BLUE
+	private float dotSizeMin = 5;// 最小的圆点大小，设置默认值为：5
+	private float dotSizeMax = 20;// 最大圆点大小，设置默认值为：20
+	private int dotCount = 8;// 圆点数量，设置默认值为：8
+	private float[] eachDotSize = null;
+	private float r = 0;
+
+	@SuppressLint("NewApi")
+	public DotCircleProgressView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		// TODO Auto-generated constructor stub
+		init(context, attrs);
+	}
+
+	public DotCircleProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+		init(context, attrs);
+	}
+
+	public DotCircleProgressView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		init(context, attrs);
+	}
+
+	public DotCircleProgressView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		init(context, null);
+	}
+
+	/***
+	 * 初始化大小、数量、颜色
+	 * 
+	 * @param attrs
+	 */
+	private void init(Context context, AttributeSet attrs) {
+		// TODO Auto-generated method stub
+		if (attrs != null) {
+			TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DotCircleProgressViewStyle);
+			// 获取圆点的颜色
+			dotColor = typedArray.getColor(R.styleable.DotCircleProgressViewStyle_dotColor, Color.BLUE);
+			// 获取最小圆点的大小
+			dotSizeMin = typedArray.getDimension(R.styleable.DotCircleProgressViewStyle_dotSizeMin, 5);
+			// 获取最大圆点的大小
+			dotSizeMax = typedArray.getDimension(R.styleable.DotCircleProgressViewStyle_dotSizeMax, 20);
+			// 获取圆点的数量
+			dotCount = typedArray.getInt(R.styleable.DotCircleProgressViewStyle_dotCount, 8);
+			typedArray.recycle();
+		}
+		mPaint = new Paint();
+		mPaint.setColor(dotColor);
+		// 抗锯齿
+		mPaint.setAntiAlias(true);
+		// 设置填充
+		mPaint.setStyle(Style.FILL);
+		eachDotSize = new float[dotCount];
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// TODO Auto-generated method stub
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	private float increment = 0;
+
+	private double degree = PI / 2;
+	private double dds = 0;
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+
+		// 如果第0位为0，那么就判定它为空。
+		// 因为onDraw()函数中是必须低消耗的，那么这样就减少for循环的次数始终为1
+		// 本来这里是打算用动画进行旋转的，根本不会有多次invalidate()，
+		// 也就不会有多次onDraw()，那以后如果想用线程控制旋转呢？呵呵
+		if (increment == 0) {
+			increment = (dotSizeMax - dotSizeMin) / dotCount;
+		}
+		if (dds == 0) {
+			dds = PI / (dotCount / 2);
+		}
+		if (eachDotSize[0] == 0) {
+			for (int i = 0; i < dotCount; i++) {
+				if (i == 0) {
+					eachDotSize[0] = dotSizeMin;
+				} else if (i == dotCount - 1) {
+					eachDotSize[i] = dotSizeMax;
+				} else {
+					eachDotSize[i] = dotSizeMin + increment * i;
+				}
+			}
+		}
+		if (r == 0) {
+			r = getMeasuredWidth() / 2 - dotSizeMax;// 半径
+		}
+
+		for (int i = 0; i < dotCount; i++) {
+			float x = getMeasuredWidth() / 2 + (float) (r * cos(degree));
+			float y = getMeasuredWidth() / 2 - (float) (r * sin(degree));
+			canvas.drawCircle(x, y, eachDotSize[i] / 2, mPaint);
+			degree = degree + dds;
+		}
+
+	}
+
+}
